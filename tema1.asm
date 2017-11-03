1eef97ae2b220c598704e6f10beef0f081e6edbc
Vlad Nicola <nicola.vlad98@gmail.com>
2017-11-03 08:59:59 +0200

    Conditii baza, transformare, printare

--- tema1.asm
+++ tema1.asm
@@ -3,8 +3,7 @@
 section .data
    %include "input.inc"
    incorect db "Baza incorecta"
-   zecimal db "0123456789", 0   ; Sirul de caractere pentru baza 10
-   hexa db "abcdef", 0          ; Sirul de caractere pana la baza 16
+   caractere db "0123456789abcdef", 0   ; Sirul de caractere folosite pentru transformare
     
 section .text
 global CMAIN
@@ -14,17 +13,36 @@ CMAIN:
     mov ecx, -1                 ; Pentru a putea incrementa in memorare
     PRINT_CHAR [hexa]           ; Apelezi cu adresa + offset
     
-memorare:
+iterare:
     INC ecx
     mov ebx, [base_array + 4 * ecx]
+    
+    cmp ebx, 2                  ;Verific daca am baza corecta
+    jl bazainc
+    cmp ebx, 16
+    jg bazainc
+    
     cmp ecx, nums + 1           ; Verific daca am iterat toate elementele  
     je final                    
 
-transformare:  
+memorare:
     mov dx, [num + 4*ecx + 2]   ; Pentru a lua partea high
     mov ax, [num + 4*ecx]       ; Partea low o retin in ax because Little Endian
     
+transformare:
     div bx
+    PRINT_CHAR [caractere + dx]
+    
+    cmp ax, 0
+    je iterare
+    
+    
+    jmp transformare            ; Fac loop pana cand transform tot numarul
+    
     
     ; Cand o sa transformi restul in caracter sa verifici de fiecare data
     ; Daca este ax = 0 dupa printare, pentru a putea itera elementele 
+
+bazainc:
+    PRINT_STRING incorect
+    jmp iterare                ; Trec la urmatorul element
