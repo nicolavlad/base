1f4db4032f3d7ce09b55f4f23268e27d89eb81b7
Vlad Nicola <nicola.vlad98@gmail.com>
2017-11-03 09:11:42 +0200

    Finalizat, dar nu ma lasa sa printez

--- tema1.asm
+++ tema1.asm
@@ -1,7 +1,10 @@
 %include "io.inc"
 
 section .data
-   %include "input.inc"
+   ;%include "input.inc"
+   nums dd 4
+   nums_array dd 612, 1330, 7, 12988
+   base_array dd 16, 1, 2, 14
    incorect db "Baza incorecta"
    caractere db "0123456789abcdef", 0   ; Sirul de caractere folosite pentru transformare
     
@@ -17,13 +20,13 @@ iterare:
     INC ecx
     mov ebx, [base_array + 4 * ecx]
     
-    cmp ebx, 2                  ;Verific daca am baza corecta
+    cmp ebx, 2                  ; Verific daca am baza corecta
     jl bazainc
     cmp ebx, 16
     jg bazainc
     
-    cmp ecx, nums + 1           ; Verific daca am iterat toate elementele  
-    je final                    
+    cmp ecx, nums + 1           ; Verific daca am iterat toate elementele
+    ret                         ; In caz de se verifica, se termina executia 
 
 memorare:
     mov dx, [num + 4*ecx + 2]   ; Pentru a lua partea high
@@ -33,9 +36,13 @@ transformare:
     div bx
     PRINT_CHAR [caractere + dx]
     
-    cmp ax, 0
+    cmp ax, 0                   ; Am terminat transformarea, pentru asta punem newline
+    NEWLINE
+    
+    cmp ax, 0                   ; Trecem la urmatorul numar
     je iterare
     
+    mov dx, 0
     
     jmp transformare            ; Fac loop pana cand transform tot numarul
     
