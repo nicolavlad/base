14b20bbf6db8ff9d466e37beeaef71652500a0e3
Vlad Nicola <nicola.vlad98@gmail.com>
2017-11-03 09:15:56 +0200

    Nu ma lasa sa fac offset cu registru de 16biti

--- tema1.asm
+++ tema1.asm
@@ -14,7 +14,6 @@ CMAIN:
     xor eax, eax
     xor edx, edx
     mov ecx, -1                 ; Pentru a putea incrementa in memorare
-    PRINT_CHAR [hexa]           ; Apelezi cu adresa + offset
     
 iterare:
     INC ecx
@@ -29,8 +28,8 @@ iterare:
     ret                         ; In caz de se verifica, se termina executia 
 
 memorare:
-    mov dx, [num + 4*ecx + 2]   ; Pentru a lua partea high
-    mov ax, [num + 4*ecx]       ; Partea low o retin in ax because Little Endian
+    mov dx, [nums_array + 4*ecx + 2]
+    mov ax, [nums_array + 4*ecx]
     
 transformare:
     div bx
@@ -46,10 +45,6 @@ transformare:
     
     jmp transformare            ; Fac loop pana cand transform tot numarul
     
-    
-    ; Cand o sa transformi restul in caracter sa verifici de fiecare data
-    ; Daca este ax = 0 dupa printare, pentru a putea itera elementele 
-
 bazainc:
     PRINT_STRING incorect
     jmp iterare                ; Trec la urmatorul element
